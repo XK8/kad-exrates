844e9fde2afa8242f621021870ac10125a5ba1d8
K <>
2019-11-23 20:06:35 +0200

    Add routes and CurrencyController

---
+++ app/Http/Controllers/CurrencyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rate;
+use Illuminate\Routing\Controller;
+
+class CurrencyController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Rate::whereNotNull('updated_at')->get(['id', 'alphabetic_code', 'name', 'english_name', 'digit_code', 'rate']));
+    }
+
+    public function show(Rate $rate)
+    {
+        return response()->json($rate);
+    }
+}
